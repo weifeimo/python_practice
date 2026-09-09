9a5d3f5cae7391e26d639fde0238121080ae1c2e
Q. ENNA <61851857+weifeimo@users.noreply.github.com>
2026-09-09 15:44:20 +0900

    finished No.207 (Arranged ver.)

--- yukicoder_207.py
+++ yukicoder_207.py
@@ -1,11 +1,8 @@
 """
  https://yukicoder.me/problems/no/207
-A以上B以下の整数のうち、3の倍数および3の付く数を、
-小さい順に出力してください。なお、「3の付く数」とは、
-10進数表記にした時、少なくとも1つの桁が3であるような数のことです。
 """
 #Checkers for A,B's range, value and distance
-def Input_range_checker(x):
+def input_range_checker(x):
     if x > 2000000000 or x < 1:
         print(f"\n入力値が範囲外です。\n1～{2000000000:,}の範囲内で再入力してください。\n")
         y = False
@@ -13,7 +10,7 @@ def Input_range_checker(x):
         y = True
     return y
 
-def Input_value_checker(x):
+def input_value_checker(a, b):
     if a >= b:
         print("\nA以上の整数Bを再入力してください。\n")
         y = False
@@ -21,7 +18,7 @@ def Input_value_checker(x):
         y = True
     return y
 
-def Input_distance_checker(x):
+def input_distance_checker(a, b):
     if b - a >= 100:
         print("\nAとBの差が大きすぎます。\n差が100以下の値を再入力してください。\n")
         y = False
@@ -33,22 +30,30 @@ def Input_distance_checker(x):
 #Start of the program
 print("正整数A、Bの値を入力してください。（A≤B）\n")
 
-valid_A = False
-valid_B = False
+valid_a = False
+valid_b = False
 
-while valid_A == False:
+valid_b1 = False    #valid_b_step1~3
+valid_b2 = False
+valid_b3 = False
+
+
+while valid_a == False:
     a = int(input("A: "))
-    valid_A = Input_range_checker(a)
-    if valid_A == True:
+    valid_a = input_range_checker(a)
+    if valid_a == True:
         break
     else:
         continue
-while valid_B == False:
+while valid_b == False:
     b = int(input("B: "))        
-    Input_range_checker(b)
-    Input_value_checker(b)
-    valid_B = Input_distance_checker(b)
-    if valid_B == True:
+
+    valid_b1 = input_range_checker(b)
+    valid_b2 = input_value_checker(a, b)
+    valid_b3 = input_distance_checker(a, b)
+    valid_b = all([valid_b1, valid_b2, valid_b3])
+
+    if valid_b == True:
         break
     else:
         continue
